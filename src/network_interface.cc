4f0d1dee808d8b65eb79a6344b5f6c7192fb9d9f
JoyousMe6 <2197792091@qq.com>
2025-12-17 15:26:58 +0800

    translate

--- src/network_interface.cc
+++ src/network_interface.cc
@@ -9,8 +9,8 @@
 
 using namespace std;
 
-//! \param[in] ethernet_address Ethernet (what ARP calls "hardware") address of the interface
-//! \param[in] ip_address IP (what ARP calls "protocol") address of the interface
+//! \param[in] ethernet_address 接口的以太网（ARP称之为“硬件”）地址
+//! \param[in] ip_address 接口的IP（ARP称之为“协议”）地址
 NetworkInterface::NetworkInterface( string_view name,
                                     shared_ptr<OutputPort> port,
                                     const EthernetAddress& ethernet_address,
@@ -24,10 +24,9 @@ NetworkInterface::NetworkInterface( string_view name,
        << ip_address.ip() << "\n";
 }
 
-//! \param[in] dgram the IPv4 datagram to be sent
-//! \param[in] next_hop the IP address of the interface to send it to (typically a router or default gateway, but
-//! may also be another host if directly connected to the same network as the destination) Note: the Address type
-//! can be converted to a uint32_t (raw 32-bit IP address) by using the Address::ipv4_numeric() method.
+//! \param[in] dgram 要发送的IPv4数据报
+//! \param[in] next_hop 要发送数据报的接口的IP地址（通常是路由器或默认网关，但如果与目标直接连接，也可以是另一台主机）
+//! 注意：Address 类型可以通过使用 Address::ipv4_numeric() 方法转换为 uint32_t（原始的32位IP地址）。
 void NetworkInterface::send_datagram( InternetDatagram dgram, const Address& next_hop )
 {
   debug( "unimplemented send_datagram called" );
@@ -35,14 +34,14 @@ void NetworkInterface::send_datagram( InternetDatagram dgram, const Address& nex
   (void)next_hop;
 }
 
-//! \param[in] frame the incoming Ethernet frame
+//! \param[in] frame 传入的以太网帧
 void NetworkInterface::recv_frame( EthernetFrame frame )
 {
   debug( "unimplemented recv_frame called" );
   (void)frame;
 }
 
-//! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
+//! \param[in] ms_since_last_tick 自上次调用该方法以来的毫秒数
 void NetworkInterface::tick( const size_t ms_since_last_tick )
 {
   debug( "unimplemented tick({}) called", ms_since_last_tick );
--- src/network_interface.hh
+++ src/network_interface.hh
@@ -7,79 +7,67 @@
 #include <memory>
 #include <queue>
 
-// A "network interface" that connects IP (the internet layer, or network layer)
-// with Ethernet (the network access layer, or link layer).
+// 一个“网络接口”，它连接 IP（互联网层，或网络层）与以太网（网络接入层，或链路层）。
 
-// This module is the lowest layer of a TCP/IP stack
-// (connecting IP with the lower-layer network protocol,
-// e.g. Ethernet). But the same module is also used repeatedly
-// as part of a router: a router generally has many network
-// interfaces, and the router's job is to route Internet datagrams
-// between the different interfaces.
+// 该模块是 TCP/IP 堆栈的最底层部分
+// （连接 IP 与更低层的网络协议，例如以太网）。但这个模块也可以反复使用
+// 作为路由器的一部分：一个路由器通常有多个网络接口，路由器的工作是路由互联网数据报在不同接口之间。
 
-// The network interface translates datagrams (coming from the
-// "customer," e.g. a TCP/IP stack or router) into Ethernet
-// frames. To fill in the Ethernet destination address, it looks up
-// the Ethernet address of the next IP hop of each datagram, making
-// requests with the [Address Resolution Protocol](\ref rfc::rfc826).
-// In the opposite direction, the network interface accepts Ethernet
-// frames, checks if they are intended for it, and if so, processes
-// the the payload depending on its type. If it's an IPv4 datagram,
-// the network interface passes it up the stack. If it's an ARP
-// request or reply, the network interface processes the frame
-// and learns or replies as necessary.
+// 网络接口将数据报（来自“客户”，例如 TCP/IP 堆栈或路由器）转换成以太网帧。
+// 为了填充以太网目标地址，它查找下一个 IP 跳跃的以太网地址，并使用地址解析协议（ARP）进行请求。
+// 反向方向，网络接口接收以太网帧，检查它们是否是发送给它的，如果是，则根据其类型处理有效载荷。
+// 如果是 IPv4 数据报，网络接口会将其推送到堆栈中。如果是 ARP 请求或回复，网络接口会处理该帧并根据需要学习或回复。
 class NetworkInterface
 {
 public:
-  // An abstraction for the physical output port where the NetworkInterface sends Ethernet frames
+  // 网络接口的物理输出端口抽象层
   class OutputPort
   {
   public:
+    // 传输以太网帧
     virtual void transmit( const NetworkInterface& sender, const EthernetFrame& frame ) = 0;
     virtual ~OutputPort() = default;
   };
 
-  // Construct a network interface with given Ethernet (network-access-layer) and IP (internet-layer)
-  // addresses
+  // 构造一个具有给定以太网（网络接入层）和 IP（互联网层）地址的网络接口
   NetworkInterface( std::string_view name,
                     std::shared_ptr<OutputPort> port,
                     const EthernetAddress& ethernet_address,
                     const Address& ip_address );
 
-  // Sends an Internet datagram, encapsulated in an Ethernet frame (if it knows the Ethernet destination
-  // address). Will need to use [ARP](\ref rfc::rfc826) to look up the Ethernet destination address for the next
-  // hop. Sending is accomplished by calling `transmit()` (a member variable) on the frame.
+  // 发送一个互联网数据报，封装在以太网帧中（如果知道以太网目标地址）。  
+  // 如果目标以太网地址未知，则通过 ARP 查找目标地址。
   void send_datagram( InternetDatagram dgram, const Address& next_hop );
 
-  // Receives an Ethernet frame and responds appropriately.
-  // If type is IPv4, pushes the datagram to the datagrams_in queue.
-  // If type is ARP request, learn a mapping from the "sender" fields, and send an ARP reply.
-  // If type is ARP reply, learn a mapping from the "sender" fields.
+  // 接收一个以太网帧并适当地响应。
+  // 如果类型是 IPv4，推送数据报到 datagrams_received 队列。
+  // 如果类型是 ARP 请求，则学习发送方的映射，并发送 ARP 回复。
+  // 如果类型是 ARP 回复，则学习发送方的映射。
   void recv_frame( EthernetFrame frame );
 
-  // Called periodically when time elapses
+  // 随着时间推移周期性地调用，用于过期已失效的 IP 到以太网的映射
   void tick( size_t ms_since_last_tick );
 
-  // Accessors
+  // 访问器
   const std::string& name() const { return name_; }
   const OutputPort& output() const { return *port_; }
   OutputPort& output() { return *port_; }
   std::queue<InternetDatagram>& datagrams_received() { return datagrams_received_; }
 
 private:
-  // Human-readable name of the interface
+  // 接口的可读名称
   std::string name_;
 
-  // The physical output port (+ a helper function `transmit` that uses it to send an Ethernet frame)
+  // 输出端口的共享指针 (+ 一个帮助函数 `transmit`，用于通过该端口发送以太网帧)
   std::shared_ptr<OutputPort> port_;
   void transmit( const EthernetFrame& frame ) const { port_->transmit( *this, frame ); }
 
-  // Ethernet (known as hardware, network-access-layer, or link-layer) address of the interface
+  // 接口的以太网（硬件，网络接入层，或链路层）地址
   EthernetAddress ethernet_address_;
 
-  // IP (known as internet-layer or network-layer) address of the interface
+  // 接口的 IP（互联网层或网络层）地址
   Address ip_address_;
 
-  // Datagrams that have been received
+  // 接收到的数据报队列
   std::queue<InternetDatagram> datagrams_received_ {};
 };
